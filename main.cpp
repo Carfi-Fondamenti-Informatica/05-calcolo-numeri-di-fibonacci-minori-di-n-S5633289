9a6e08ffb390d0c0d60d289f399ec7a8693a7303
S5633289 <118168182+S5633289@users.noreply.github.com>
2022-11-27 12:46:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int a){
+    int n1=0,n2=1,n3;
+    cout<<"1"<<endl;
+    for(int i=2;i<=a;i++){
+        n3=n1+n2;
+        if(n3<=a){
+        cout<<n3<<endl;
+        }
+        n1=n2;
+        n2=n3;
+    }
+    return 0;
+}
+
 int main() {
-   return 0;
+    int n;
+    cin>>n;
+    if(n>=2){
+        fibonacci(n);
+    }
+    else{ cout<<"errore";}
+    return 0;
 }
